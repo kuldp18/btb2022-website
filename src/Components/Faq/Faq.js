10b01f482666dc950f52f372a4a45e7bbbb51bfa
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-04-02 19:14:56 +0530

    fixed the triangle things and added final responsive stuff for the about section

--- src/Components/Faq/Faq.js
+++ src/Components/Faq/Faq.js
@@ -3,126 +3,149 @@ import './../../Styles/Faq.css';
 
 export const Faq = () => {
   return (
-    <section className="Faq" id="faq">
-      <div class="container1">
-        <div class="accordion1">
-          <div class="title1">FAQs</div>
-          <div class="accordion-item1" id="question1">
-            <a class="accordion-link1" href="#question1">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                impedit officia accusantium eius nihil magnam architecto ea
-                error beatae voluptatibus ducimus reiciendis ad aut magni quia
-                sapiente, earum, corporis repellendus adipisci natus nemo quas.
-                Incidunt repellendus dolores asperiores quaerat officia iure
-                laudantium, ipsa aut illum. Facilis, molestiae fuga ut nesciunt
-                alias dicta impedit ex aut.
-              </p>
+    <>
+      <svg
+        id="wave"
+        style={{ transform: 'rotate(0deg)', transition: '0.3s' }}
+        viewBox="0 0 1440 100"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0">
+            <stop stopColor="rgba(52, 43, 63, 1)" offset="0%"></stop>
+            <stop stopColor="rgba(52, 43, 63, 1)" offset="100%"></stop>
+          </linearGradient>
+        </defs>
+        <path
+          style={{ transform: 'translate(0, 0px)', opacity: '1' }}
+          fill="#21192B"
+          d="M0,30L288,80L576,60L864,40L1152,60L1440,20L1728,10L2016,20L2304,90L2592,50L2880,10L3168,30L3456,70L3744,0L4032,50L4320,70L4608,80L4896,0L5184,80L5472,0L5760,20L6048,40L6336,30L6624,40L6912,70L6912,100L6624,100L6336,100L6048,100L5760,100L5472,100L5184,100L4896,100L4608,100L4320,100L4032,100L3744,100L3456,100L3168,100L2880,100L2592,100L2304,100L2016,100L1728,100L1440,100L1152,100L864,100L576,100L288,100L0,100Z"
+        ></path>
+      </svg>
+      <section className="Faq" id="faq">
+        <div class="container1">
+          <div class="accordion1">
+            <div class="title1">FAQs</div>
+            <div class="accordion-item1" id="question1">
+              <a class="accordion-link1" href="#question1">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Tempore impedit officia accusantium eius nihil magnam
+                  architecto ea error beatae voluptatibus ducimus reiciendis ad
+                  aut magni quia sapiente, earum, corporis repellendus adipisci
+                  natus nemo quas. Incidunt repellendus dolores asperiores
+                  quaerat officia iure laudantium, ipsa aut illum. Facilis,
+                  molestiae fuga ut nesciunt alias dicta impedit ex aut.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="accordion-item1" id="question2">
-            <a class="accordion-link1" href="#question2">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Assumenda aliquam autem dolores, adipisci, velit voluptas
-                debitis nam eveniet tempora ad error ex eaque! Dicta error
-                officiis aperiam aspernatur iure, voluptate fugiat autem
-                repellat dolor aut ipsam tenetur odio maxime rem molestias.
-                Iusto maiores, consequatur aliquid nisi deserunt repellat libero
-                quam eligendi perspiciatis labore dolore magnam.
-              </p>
+            <div class="accordion-item1" id="question2">
+              <a class="accordion-link1" href="#question2">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Assumenda aliquam autem dolores, adipisci, velit voluptas
+                  debitis nam eveniet tempora ad error ex eaque! Dicta error
+                  officiis aperiam aspernatur iure, voluptate fugiat autem
+                  repellat dolor aut ipsam tenetur odio maxime rem molestias.
+                  Iusto maiores, consequatur aliquid nisi deserunt repellat
+                  libero quam eligendi perspiciatis labore dolore magnam.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="accordion-item1" id="question3">
-            <a class="accordion-link1" href="#question3">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Tenetur, fugit. Consectetur recusandae commodi dolor distinctio
-                porro sit aliquid deleniti facilis quasi eligendi numquam quo
-                eveniet a, reprehenderit sequi. Necessitatibus iure at, et
-                temporibus eligendi veritatis earum nulla, assumenda tempore
-                dolorem voluptatem minima aperiam consectetur autem ipsum
-                quibusdam quae quis sunt. Et perferendis optio quaerat eum.
-              </p>
+            <div class="accordion-item1" id="question3">
+              <a class="accordion-link1" href="#question3">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Tenetur, fugit. Consectetur recusandae commodi dolor
+                  distinctio porro sit aliquid deleniti facilis quasi eligendi
+                  numquam quo eveniet a, reprehenderit sequi. Necessitatibus
+                  iure at, et temporibus eligendi veritatis earum nulla,
+                  assumenda tempore dolorem voluptatem minima aperiam
+                  consectetur autem ipsum quibusdam quae quis sunt. Et
+                  perferendis optio quaerat eum.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="accordion-item1" id="question4">
-            <a class="accordion-link1" href="#question4">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
-                exercitationem quia odit at cum voluptates veniam temporibus
-                numquam perspiciatis, sequi, cupiditate natus eos minus
-                accusantium sit praesentium hic fuga facilis sapiente ut
-                obcaecati ullam! Cumque, dolorum ducimus eius ratione rem minima
-                numquam, fugiat similique itaque in reprehenderit accusantium
-                error corporis molestias nemo, tenetur aut expedita.
-              </p>
+            <div class="accordion-item1" id="question4">
+              <a class="accordion-link1" href="#question4">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+                  exercitationem quia odit at cum voluptates veniam temporibus
+                  numquam perspiciatis, sequi, cupiditate natus eos minus
+                  accusantium sit praesentium hic fuga facilis sapiente ut
+                  obcaecati ullam! Cumque, dolorum ducimus eius ratione rem
+                  minima numquam, fugiat similique itaque in reprehenderit
+                  accusantium error corporis molestias nemo, tenetur aut
+                  expedita.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="accordion-item1" id="question5">
-            <a class="accordion-link1" href="#question5">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Temporibus molestiae alias repudiandae quisquam nemo, placeat
-                animi at, minima aspernatur quidem ipsum deleniti itaque numquam
-                vero laboriosam fuga! Dolor recusandae ad deleniti, totam autem
-                facilis quaerat id esse dolorem dicta provident sed hic
-                assumenda illum fugit impedit similique, natus qui tempora
-                laborum. Voluptatibus id maiores necessitatibus!
-              </p>
+            <div class="accordion-item1" id="question5">
+              <a class="accordion-link1" href="#question5">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Temporibus molestiae alias repudiandae quisquam nemo, placeat
+                  animi at, minima aspernatur quidem ipsum deleniti itaque
+                  numquam vero laboriosam fuga! Dolor recusandae ad deleniti,
+                  totam autem facilis quaerat id esse dolorem dicta provident
+                  sed hic assumenda illum fugit impedit similique, natus qui
+                  tempora laborum. Voluptatibus id maiores necessitatibus!
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div class="accordion-item1" id="question6">
-            <a class="accordion-link1" href="#question6">
-              Q. Can we adjust the length of wire?
-              <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
-              <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
-            </a>
-            <div class="answer1">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-                necessitatibus maxime veritatis, excepturi libero reiciendis,
-                optio eligendi nostrum id quasi laudantium placeat corrupti
-                inventore. Repellendus rerum vitae consectetur laboriosam, ipsa,
-                nobis aliquam incidunt veniam nisi cum consequuntur? Similique,
-                odio ut! Voluptas voluptates, rem distinctio, dolor dicta
-                repellendus magnam quis provident iste culpa quam. Dolore,
-                assumenda!
-              </p>
+            <div class="accordion-item1" id="question6">
+              <a class="accordion-link1" href="#question6">
+                Q. Can we adjust the length of wire?
+                <ion-icon name="chevron-down-outline" class="down1"></ion-icon>
+                <ion-icon name="chevron-up-outline" class="up1"></ion-icon>
+              </a>
+              <div class="answer1">
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Omnis necessitatibus maxime veritatis, excepturi libero
+                  reiciendis, optio eligendi nostrum id quasi laudantium placeat
+                  corrupti inventore. Repellendus rerum vitae consectetur
+                  laboriosam, ipsa, nobis aliquam incidunt veniam nisi cum
+                  consequuntur? Similique, odio ut! Voluptas voluptates, rem
+                  distinctio, dolor dicta repellendus magnam quis provident iste
+                  culpa quam. Dolore, assumenda!
+                </p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
