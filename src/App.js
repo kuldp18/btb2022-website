cddb315ce29d4416621375e9c8061c83a376d954
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-04-17 10:45:57 +0530

    minor change:added scroll to top button

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ import { Footer } from './Components/Footer/Footer';
 import { Faq } from './Components/Faq/Faq';
 import { Home } from './Components/Home/Home';
 import Sponsors from './Components/Sponsors/Sponsors';
+import ScrollToTop from 'react-scroll-to-top';
 
 function App() {
   return (
     <>
+      <ScrollToTop
+        smooth
+        color="#e12f26"
+        top={150}
+        width="30"
+        height="30"
+        style={{
+          backgroundColor: '#21192b',
+          border: '2px solid rgba(255,255,255,0.8)',
+          borderRadius: '50%',
+        }}
+      />
       <Home />
       <AboutUs />
       <Events />
