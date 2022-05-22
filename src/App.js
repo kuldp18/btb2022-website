1e79c945e356161a02eeb2c434aefc3238be14c1
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-05-22 12:54:13 +0530

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import './App.css';
-import Events from './Components/Events/Events';
-import TimeLine from './Components/TimeLine/TimeLine';
-import { AboutUs } from './Components/AboutUs/AboutUs';
 import '@fontsource/metropolis';
-import { Footer } from './Components/Footer/Footer';
-import { Faq } from './Components/Faq/Faq';
 import { Home } from './Components/Home/Home';
-import Sponsors from './Components/Sponsors/Sponsors';
 import ScrollToTop from 'react-scroll-to-top';
 import TeamSection from './Components/TeamSection';
 import { IoIosArrowUp } from 'react-icons/io';
