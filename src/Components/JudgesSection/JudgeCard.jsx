d276560b3741f2fd601fcde4a7b3b0d1fbc08447
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-05-30 22:02:20 +0530

    completed judges section

--- src/Components/JudgesSection/JudgeCard.jsx
+++ src/Components/JudgesSection/JudgeCard.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../../Styles/JudgesSection.css';
 
-const JudgeCard = ({ name, work, company }) => {
+const JudgeCard = ({ name, work, company, image }) => {
   return (
     <div className="judges__card">
       <div className="judges__card__top">
-        <div className="judge__image"></div>
+        <div className="judge__image">
+          <img src={image} alt={name} />
+        </div>
       </div>
       <div className="judges__card__bottom">
-        <h2 className="judge__name">Atul Khola</h2>
-        <p className="judge__position">Product and Design</p>
+        <h2 className="judge__name">{name}</h2>
+        <p className="judge__position">{work}</p>
         <p className="judge__company">
-          at <span className="judge__span">CRED</span>
+          <span className="judge__span">{company}</span>
         </p>
       </div>
     </div>
--- src/Components/JudgesSection/index.jsx
+++ src/Components/JudgesSection/index.jsx
@@ -7,25 +7,68 @@ import Separator2 from '../Separator2';
 const JudgesSection = () => {
   return (
     <>
-      <Separator />
+      {/* <Separator /> */}
       <section className="judges" id="judges">
         <h2 className="judges__title">Meet the Judges</h2>
         <div className="judges-grid">
-          <JudgeCard />
-          <JudgeCard />
-          <JudgeCard />
-          <JudgeCard />
-          <JudgeCard />
+          <JudgeCard
+            name="Anjali Sheel"
+            work="Software Engineer II"
+            company="MICROSOFT"
+            image={require('../../require_images/judges/Anjali_Sheel.png')}
+          />
+          <JudgeCard
+            name="Shabnam"
+            work="Associate Project Manager"
+            company="MICROSOFT"
+            image={require('../../require_images/judges/Shabnam.png')}
+          />
+          <JudgeCard
+            name="Luis Ouriach"
+            work="Design Advocate"
+            company="FIGMA"
+            image={require('../../require_images/judges/Luios.png')}
+          />
+          <JudgeCard
+            name="Kunal Lagwankar"
+            work="Director of Product Management"
+            company="WARNER BROS. DISCOVERY"
+            image={require('../../require_images/judges/Kunal.png')}
+          />
+          <JudgeCard
+            name="Atul Khola"
+            work="Product and Design"
+            company="CRED"
+            image={require('../../require_images/judges/Atul.png')}
+          />
+          <JudgeCard
+            name="Jaydip Dey"
+            work="Chapter Lead"
+            company="GIRLSCRIPT"
+            image={require('../../require_images/judges/Jaydip.png')}
+          />
+          <JudgeCard
+            name="Deepanshu Jain"
+            work="Software Engineer"
+            company="ATLASSIAN"
+            image={require('../../require_images/judges/Deepanshu.png')}
+          />
+          <JudgeCard
+            name="Udit Agarwal"
+            work="Software Development Manager III"
+            company="AMAZON"
+            image={require('../../require_images/judges/Udit.png')}
+          />
         </div>
       </section>
-      <Separator2
+      {/* <Separator2
         styles={{
           width: '100%',
           maxHeight: '200px',
           height: '100%',
           zIndex: '99',
         }}
-      />
+      /> */}
     </>
   );
 };
