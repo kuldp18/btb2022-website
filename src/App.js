43fe31fae1258624101ad499a9ca58d3c4c9ba5f
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-07-26 21:53:17 +0530

    added correct images finally

--- src/App.js
+++ src/App.js
@@ -261,7 +261,7 @@ function App() {
               workShopDetails=""
               workShopDate="05/08/2022"
               workShopLink="Coming Soon"
-              imgPath={require('./require_images/Workshop-longnames.png')}
+              imgPath={require('./require_images/Workshop-KESAV AND AASAWARI.png')}
               workShopTime="12PM TO 1PM"
             />
           }
@@ -275,7 +275,7 @@ function App() {
               workShopDetails=""
               workShopDate="07/08/2022"
               workShopLink="Coming Soon"
-              imgPath={require('./require_images/Workshop-Ishika.png')}
+              imgPath={require('./require_images/Workshop-ISHIKA.png')}
               workShopTime="3PM TO 5PM"
             />
           }
