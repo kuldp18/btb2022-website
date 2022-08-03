4d7131608ca9b0e339820b5211ae412614adf937
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2022-08-03 18:41:01 +0530

    added Faq data in a separate file

---
+++ src/Components/Faq/faqdata.js
@@ -0,0 +1,50 @@
+export const Data = [
+  {
+    question: 'Q. Who can participate?',
+    answer:
+      'Anyone who is up for challenges and looking forward to learning something new is welcome to apply. However, the participant should be a student of any college/university in India.',
+  },
+  {
+    question: 'Q. Is there any participation fee?',
+    answer: 'No, there is no participation fee.',
+  },
+  {
+    question: 'Q. How do I apply?',
+    answer:
+      'We will be adding the registration link soon. Stay connected with us on our social media for more updates.',
+  },
+  {
+    question: 'Q. Can I participate remotely?',
+    answer: 'Yes, the workshops and the hackathons are completely online.',
+  },
+  {
+    question: 'Q. How many participants are allowed in one team?',
+    answer:
+      'You are welcome to work alone, but we recommend working with a team. A team for this hackathon can be set from an individual member and limited to a max of 4 members.',
+  },
+  {
+    question: 'Q. Can I participate in more than one tracks?',
+    answer:
+      'We recommend you participate in only one track and put your best foot forward. However, you can participate in as many tracks as you want according to your interests.',
+  },
+  {
+    question: 'Q. How does an online Hackathon works?',
+    answer:
+      'You don’t have to be online for 48 hours! We will be providing the problem statements then you can start your project and hack with your team however you would like.',
+  },
+  {
+    question: 'Q. Where to submit our work?',
+    answer:
+      'We will be taking the project submissions on deposit and you are required to submit a video presentation of your project via YouTube.',
+  },
+  {
+    question: "Q. I can't code, can I still join?",
+    answer:
+      'Of course! Our hackathon is open to all, from beginners to more advanced students! We will be hosting several workshops to help you learn and enhance your skills. Remember, anyone can code, we just need a path to follow!',
+  },
+  {
+    question: 'Q. What if I still have questions?',
+    answer:
+      "<p> Shoot your questions on our{' '} <a href=\"https://discord.gg/uN3dhWBKKT\" target=\"_blank\" style={{ color: '#ff5148', }} > discord </a>{' '} and we will surely get back to you as soon as we can. </p>",
+  },
+];
